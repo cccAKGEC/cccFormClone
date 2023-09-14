511df65df3fc05b3192bad2eded0d019f985e83b
RISHABH GUPTA <104566113+Rishabh2503@users.noreply.github.com>
2023-09-14 23:05:55 +0530

    Adding some changes

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,8 +9,9 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     GithubProvider({
-      clientId:process.env.GITHUB_ID,
-      clientSecret:process.env.GITHUB_SECRET
-    })
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
